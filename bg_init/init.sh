09dc4070265d1a8a0d6bab5d32443ae4d4977457
Matthew Hollick <matthew@mayan-it.co.uk>
2018-04-10 12:47:47 +0100

    fixed cqlsh command in bg_init

--- bg_init/init.sh
+++ bg_init/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "this can take some time. get some tea."
-/shared/wait-for-it.sh cassandra:9042 -t 300 -- cqlsh cassandra /bg_init/schema.cql
+/shared/wait-for-it.sh cassandra:9042 -t 300 -- cqlsh --cqlversion=3.4.4 cassandra < /bg_init/schema.cql
 bgutil --cassandra_contact_points cassandra syncdb --storage-schemas /biggraphite/storage-schemas.conf
